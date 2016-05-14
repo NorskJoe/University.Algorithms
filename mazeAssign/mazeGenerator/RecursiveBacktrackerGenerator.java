aa9ad6b4f56939030697ef955d0fcb4aadf5bbd6
rmit-s3542413-joseph-johnson <s3542413@student.rmit.edu.au>
2016-05-14 18:08:22 +1000

    implemented tunnel recursive backtracker

--- mazeAssign/mazeGenerator/RecursiveBacktrackerGenerator.java
+++ mazeAssign/mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -7,8 +7,6 @@ import java.util.Stack;
 import maze.Cell;
 import maze.Maze;
 
-
-
 public class RecursiveBacktrackerGenerator implements MazeGenerator {
 
 	Stack<Cell> stack = new Stack<Cell>();
@@ -145,7 +143,130 @@ public class RecursiveBacktrackerGenerator implements MazeGenerator {
 	
 
 	private void tunnelMaze(Maze maze) {
-		// TODO Auto-generated method stub
+		Cell currentCell = null;
+		
+		// Creating boolean array to mark cells visited or unvisited
+		boolean[][] cellVisitor = new boolean[maze.sizeR][maze.sizeC];
+		
+		/*
+		 * Set the currentCell as the entrance to the maze
+		 * and push it to the stack
+		 */
+		currentCell = maze.entrance;
+		stack.push(currentCell);
+		
+		Cell cell = maze.map[3][4];
+		
+		
+		/*
+		 * Main body of function.  Recursively visit all cells marking them as 
+		 * visited and carving a path.
+		 * 
+		 * This will continue until all cells have been visited, and therefore the stack is empty
+		 */
+		do
+		{
+			// Mark the currentCell as visited
+			cellVisitor[currentCell.r][currentCell.c] = true;
+			ArrayList<String> availableNeighbours = new ArrayList<String>();
+			
+			/*
+			 *  Each if statement checks if the neighbour is within bounds, and has not been visited.
+			 *  
+			 *  The tunnel implementation also checks if a cell has a 'tunnel neighbour'.
+			 *  If the currentCell is a tunnel, then you move through the tunnel
+			 */
+			System.out.println("current cell is a tunnel? " + currentCell.tunnelTo);
+			if(currentCell.tunnelTo != null && cellVisitor[currentCell.tunnelTo.r][currentCell.tunnelTo.c] != true)
+			{
+				availableNeighbours.add("TUNNEL");
+			}
+			else
+			{
+				
+				if(currentCell.c >= 0 && currentCell.c <= maze.sizeC && currentCell.r >= 0 && currentCell.r < maze.sizeR - 1
+						&& cellVisitor[currentCell.r + 1][currentCell.c] != true)
+				{
+					availableNeighbours.add("NORTH");
+				}
+				if(currentCell.c >= 0 && currentCell.c < maze.sizeC - 1 && currentCell.r >= 0 && currentCell.r <= maze.sizeR
+						&& cellVisitor[currentCell.r][currentCell.c + 1] != true)
+				{
+					
+					availableNeighbours.add("EAST");
+				}
+				if(currentCell.c >= 0 && currentCell.c <= maze.sizeC && currentCell.r > 0 && currentCell.r <= maze.sizeR
+						&& cellVisitor[currentCell.r - 1][currentCell.c] != true)
+				{
+					
+					availableNeighbours.add("SOUTH");
+				}
+				if(currentCell.c > 0 && currentCell.c <= maze.sizeC && currentCell.r >= 0 && currentCell.r <= maze.sizeR
+						&& cellVisitor[currentCell.r][currentCell.c - 1] != true)
+				{
+					
+					availableNeighbours.add("WEST");
+				}
+			}
+				
+			System.out.println(availableNeighbours);
+			System.out.println("current row, col: " + currentCell.r + " " + currentCell.c);
+				
+			/*
+			 * If none of the neighbours are viable next cells then move back to the last cell visited
+			 */
+			if(availableNeighbours.isEmpty())
+			{
+				currentCell = stack.pop();
+			}
+
+			/*
+			 * This block will choose a random neighbour, destroy the wall between
+			 * the currentCell and the neighbour, push the currentCell to the stack
+			 *  and set the currentCell to the neighbour
+			 */
+			else
+			{				
+				Random rand = new Random();
+				String direction = availableNeighbours.get(rand.nextInt(availableNeighbours.size()));
+				
+				System.out.println("Chosen cell position is: " + direction);
+				
+				System.out.println("NORTH WALL PRESENT? " + currentCell.wall[Maze.NORTH].present);
+				System.out.println("EAST WALL PRESENT? " + currentCell.wall[Maze.EAST].present);
+				System.out.println("SOUTH WALL PRESENT? " + currentCell.wall[Maze.SOUTH].present);
+				System.out.println("WEST WALL PRESENT? " + currentCell.wall[Maze.WEST].present);
+				
+				if(direction.equals("TUNNEL"))
+				{
+					currentCell = maze.map[currentCell.tunnelTo.r][currentCell.tunnelTo.c];
+				}
+				if(direction.equals("NORTH"))
+				{
+					currentCell.wall[Maze.NORTH].present = false;
+					currentCell = maze.map[currentCell.r + 1][currentCell.c];
+				}
+				if(direction.equals("EAST"))
+				{
+					currentCell.wall[Maze.EAST].present = false;
+					currentCell = maze.map[currentCell.r][currentCell.c + 1];
+				}
+				if(direction.equals("SOUTH"))
+				{
+					currentCell.wall[Maze.SOUTH].present = false;
+					currentCell = maze.map[currentCell.r - 1][currentCell.c];
+				}
+				if(direction.equals("WEST"))
+				{
+					currentCell.wall[Maze.WEST].present = false;
+					currentCell = maze.map[currentCell.r][currentCell.c - 1];
+				}
+				
+				stack.push(currentCell);
+			}
+			
+		}while(stack.isEmpty() == false);
+		
 		
 	}// End of tunnelMaze()
 
