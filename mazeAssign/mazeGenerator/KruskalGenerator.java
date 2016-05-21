b0bb36fe308e060acfd9b4a2babd8bd385dabfcb
rmit-s3542413-joseph-johnson <s3542413@student.rmit.edu.au>
2016-05-21 12:09:36 +1000

    Started kruskals generator

--- mazeAssign/mazeGenerator/KruskalGenerator.java
+++ mazeAssign/mazeGenerator/KruskalGenerator.java
@@ -6,8 +6,38 @@ public class KruskalGenerator implements MazeGenerator {
 
 	@Override
 	public void generateMaze(Maze maze) {
-		// TODO Auto-generated method stub
+		/*
+		 * Each if statement checks what type of maze is being generated and
+		 * calls the appropriate method
+		 */
+		if (maze.type == Maze.NORMAL)
+		{
+			normalMaze(maze);
+		}
+		else if (maze.type == Maze.HEX)
+		{
+			hexMaze(maze);
+		}
+		else if (maze.type == Maze.TUNNEL)
+		{
+			tunnelMaze(maze);
+		}
 
 	} // end of generateMaze()
+
+	private void normalMaze(Maze maze) {
+		// TODO Auto-generated method stub
+		
+	}// End of normal generator
+
+	private void hexMaze(Maze maze) {
+		// TODO Auto-generated method stub
+		
+	}// End of hex generator
+
+	private void tunnelMaze(Maze maze) {
+		// TODO Auto-generated method stub
+		
+	}// Enf of tunnel generator
 
 } // end of class KruskalGenerator
