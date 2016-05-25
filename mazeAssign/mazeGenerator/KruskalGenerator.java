e98fb1abfc53f07354ac5595e9e3bfbac2c1ddb4
rmit-s3542413-joseph-johnson <s3542413@student.rmit.edu.au>
2016-05-25 19:22:51 +1000

    Kruskals tunnel generator finished
    All kruskals generators finished

--- mazeAssign/mazeGenerator/KruskalGenerator.java
+++ mazeAssign/mazeGenerator/KruskalGenerator.java
@@ -146,12 +146,15 @@ public class KruskalGenerator implements MazeGenerator {
 		 * eastern edges are not checked because it would cause some
 		 * edges to be added twice
 		 */
+		// Get the actual size of the columns, different for hex mazes
 		int realSizeC = (maze.sizeC+1)/2+maze.sizeC;
-		System.out.println(realSizeC);
-		
 		Edge pair = null;
 		for(int i = 0; i < maze.sizeR; i++)
 		{
+			/* 
+			 * tempSet is used to store all the Trees for this row.
+			 * Sets are nested for easy retrieval of cells later.
+			 */
 			ArrayList<Tree> tempSet = new ArrayList<Tree>();
 			for(int j = 0; j < realSizeC; j++)
 			{
@@ -246,8 +249,9 @@ public class KruskalGenerator implements MazeGenerator {
 			}
 		
 			/*
-			 * Always remove any edges that are checked, if they are rejected they
-			 * should be removed from the list and not considered again.
+			 * Always remove any edges that are checked, if they are rejected
+			 * (by the if-statement) they should be removed from the list 
+			 * anyway and not considered again.
 			 */
 			edges.remove(edge);
 		}
@@ -255,7 +259,112 @@ public class KruskalGenerator implements MazeGenerator {
 	}// End of hex generator
 
 	private void tunnelMaze(Maze maze) {
-		// TODO Auto-generated method stub
+		/*
+		 * First add an edge for every pair of adjacent cells to a list.
+		 * 
+		 * Iterate through the whole maze, getting all adjacent pairs and add the 
+		 * pair to the list.
+		 * Each edge is represented as an adjacent pair of cells.
+		 * 
+		 * South and east edges are not checked because it would cause some
+		 * edges to be added twice
+		 */
+		Edge pair = null;
+		for(int i = 0; i < maze.sizeR; i++)
+		{
+			ArrayList<Tree> tempSet = new ArrayList<Tree>();
+			for(int j = 0; j < maze.sizeC; j++)
+			{
+				Cell currentCell = maze.map[i][j];
+				tempSet.add(new Tree());
+				
+				if(currentCell.tunnelTo != null)
+				{
+					pair = new Edge(currentCell, currentCell.tunnelTo);
+					edges.add(pair);
+				}
+				
+				// Checking neighbour on north side of cell
+				if (currentCell.c >= 0 && currentCell.c <= maze.sizeC && currentCell.r >= 0
+						&& currentCell.r < maze.sizeR - 1)
+				{
+					pair = new Edge(currentCell, currentCell.neigh[Maze.NORTH]);
+					if(edges.contains(pair)==false)
+					{
+						edges.add(pair);						
+					}
+				}
+
+				// Checking neighbour on west side of cell
+				if (currentCell.c > 0 && currentCell.c <= maze.sizeC && currentCell.r >= 0 
+						&& currentCell.r <= maze.sizeR)
+				{
+					pair = new Edge(currentCell, currentCell.neigh[Maze.WEST]);
+					if(edges.contains(pair)==false)
+					{
+						edges.add(pair);						
+					}
+				}
+			}
+			sets.add(tempSet);
+		}
+		System.out.println(edges.size());
+		/*
+		 * Main body of algorithm
+		 * 
+		 * Continue to choose random edges until there are none left.
+		 */
+		while(!edges.isEmpty())
+		{
+			// Randomly select an edge
+			Random rand = new Random();
+			Edge edge = edges.get(rand.nextInt(edges.size()));
+			Cell cellOne = edge.cellOne;
+			Cell cellTwo = null;
+			// If the cell is part of a tunnel, make the next move go through
+			if(cellOne.tunnelTo != null)
+			{
+				cellTwo = cellOne.tunnelTo;
+			}
+			// If it is not part of a tunnel, carry on as usual
+			else
+			{				
+				cellTwo = edge.cellTwo;
+			}
+			
+			// Get the corresponding sets for the two cells
+			Tree set1 = (sets.get(cellOne.r)).get(cellOne.c);
+			Tree set2 = (sets.get(cellTwo.r)).get(cellTwo.c);
+			
+			// If the sets are not connected, carve a path and connect them
+			if(!set1.connected(set2))
+			{
+				
+				// Check in which direction the pairs are joined, starting from cellOne
+				if(cellOne.neigh[Maze.NORTH]==cellTwo)
+				{
+					cellOne.wall[Maze.NORTH].present = false;
+				}
+				else if(cellOne.neigh[Maze.EAST]==cellTwo)
+				{
+					cellOne.wall[Maze.EAST].present = false;
+				}
+				else if(cellOne.neigh[Maze.SOUTH]==cellTwo)
+				{
+					cellOne.wall[Maze.SOUTH].present = false;
+				}
+				else if(cellOne.neigh[Maze.WEST]==cellTwo)
+				{
+					cellOne.wall[Maze.WEST].present = false;
+				}
+				set1.connect(set2);
+			}
+			/*
+			 * Always remove any edges that are checked, if they are rejected they
+			 * should be removed from the list and not considered again.
+			 */
+			edges.remove(edge);
+		}
 		
 	}// End of tunnel generator
 	
@@ -275,24 +384,31 @@ public class KruskalGenerator implements MazeGenerator {
 		 * Return parent if exists, otherwise return this object instance.
 		 */
 		public Tree root() {
-			return parent != null ? parent.root() : this;
+			// Recursively get parent until you get to root
+			if(parent != null)
+			{
+				return parent.root();
+			}
+			else
+			{
+				return this;
+			}
 		}
 		
 		/*
-		 * Are we connected to this tree?
+		 * Check to see if the two sets are connected
 		 */
 		public boolean connected(Tree tree) {
 			return this.root() == tree.root();
 		}
 		
-		//
-		// Connect to the tree
-		//
+		/*
+		 * Connect two sets together
+		 */
 		public void connect(Tree tree) {
 			tree.root().setParent(this);
 		}
 		
-		//
 		// Set the parent of the object instance
 		public void setParent(Tree parent) {
 			this.parent = parent;
